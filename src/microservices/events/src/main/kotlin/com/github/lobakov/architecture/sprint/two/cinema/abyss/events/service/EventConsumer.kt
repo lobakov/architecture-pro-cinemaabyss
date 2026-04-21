4f69c3993eb278f145c66c00c9366aa0874c9b2e
Roman Lobakov <roman.lobakov@rt.ru>
2026-04-22 01:18:08 +0300

    Выполнил задание 3

--- src/microservices/events/src/main/kotlin/com/github/lobakov/architecture/sprint/two/cinema/abyss/events/service/EventConsumer.kt
+++ src/microservices/events/src/main/kotlin/com/github/lobakov/architecture/sprint/two/cinema/abyss/events/service/EventConsumer.kt
@@ -1,5 +1,6 @@
 package com.github.lobakov.architecture.sprint.two.cinema.abyss.events.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.lobakov.architecture.sprint.two.cinema.abyss.events.model.Event
 import com.github.lobakov.architecture.sprint.two.cinema.abyss.events.model.MovieEvent
 import com.github.lobakov.architecture.sprint.two.cinema.abyss.events.model.PaymentEvent
@@ -7,23 +8,29 @@ import com.github.lobakov.architecture.sprint.two.cinema.abyss.events.model.User
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
+import kotlin.jvm.java
 
 @Service
-class EventConsumer {
+class EventConsumer(
+    private val objectMapper: ObjectMapper
+) {
 
     @KafkaListener(topics = ["user-events"], groupId = "event-group")
     fun consumeUserEvent(event: Event) {
-        logger.info("Received User Event: ${event.payload as UserEvent}")
+        val userEvent = objectMapper.convertValue(event.payload, UserEvent::class.java)
+        logger.info("Received User Event: $userEvent")
     }
 
     @KafkaListener(topics = ["payment-events"], groupId = "event-group")
     fun consumePaymentEvent(event: Event) {
-        logger.info("Received Payment Event: ${event.payload as PaymentEvent}")
+        val paymentEvent = objectMapper.convertValue(event.payload, PaymentEvent::class.java)
+        logger.info("Received Payment Event: $paymentEvent")
     }
 
     @KafkaListener(topics = ["movie-events"], groupId = "event-group")
     fun consumeMovieEvent(event: Event) {
-        logger.info("Received Movie Event: ${event.payload as MovieEvent}")
+        val movieEvent = objectMapper.convertValue(event.payload, MovieEvent::class.java)
+        logger.info("Received Movie Event: $movieEvent")
     }
 
     companion object {
